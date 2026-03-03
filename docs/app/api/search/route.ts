d4150b5cff59a9ae31865065a2399c2f46a6f0e0
Cheeteau <29103374+chteau@users.noreply.github.com>
2026-03-03 13:10:21 +0100

    Update route.ts

--- docs/app/api/search/route.ts
+++ docs/app/api/search/route.ts
@@ -1,7 +1,2 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
-
-export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/docs/orama-js/supported-languages
-  language: 'english',
-});
+export { GET } from 'fumadocs-core/search/server';
+export const dynamic = 'force-static';
