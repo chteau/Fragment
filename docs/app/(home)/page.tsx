a749e0ea5dc1581ff23fe9c6e156f6fa9722d802
MartinPJB <29103374+MartinPJB@users.noreply.github.com>
2026-03-03 13:39:10 +0100

    PLEASE VERCEL

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -1,11 +1,11 @@
 "use client"
-
-import Image from 'next/image';
-import Link from 'next/link';
-import Beams from "@/components/Beams";
-import { redirect, RedirectType } from 'next/navigation'
-
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function HomePage() {
-    return redirect("/docs", RedirectType.replace);
+    const router = useRouter()
+    useEffect(() => {
+        router.replace('/docs')
+    }, [router])
+    return null
 }
