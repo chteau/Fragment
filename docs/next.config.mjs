9a86a7a8421a5744cd609144763f850b8d7dd793
Cheeteau <29103374+chteau@users.noreply.github.com>
2026-03-03 12:56:38 +0100

    Update next.config.mjs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,11 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH ?? '',
+  images: {
+    unoptimized: true, // required for static export
+  },
 };
 
 export default withMDX(config);
